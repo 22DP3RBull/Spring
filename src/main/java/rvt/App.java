7046792d061d4f49c29239e70cb0eca23f046af0
Raienss <131261213+22DP3RBull@users.noreply.github.com>
2024-02-21 08:06:07 +0000

    kkads prikols

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -5,7 +5,43 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+public static void main(String[] args) {
+SpringApplication.run(App.class, args);
+}
+
+public static void Money(){
+Money macins1 = new Money(0, 3);
+Money macins2 = new Money(0, 3);
+Money kopejais_macins = macins1.plus(macins2);
+System.out.println();
+System.out.println("Kopā abos maciņos ir " + kopejais_macins);
+System.out.println("Pirmajā maciņa ir vairāk nekā otrajā" + macins1.lessThan(macins2));
+Money Starp_macins = macins1.minus(macins2);
+System.out.println("Atņemot no pirmā maciņa otro, paliek" + Starp_macins + "eiro");
+System.out.println("pirmais un otrais maciņš ir vienāds: " + macins1.equals(macins2));
+}
+
+//public static void smallest(){
+// int[] array = {3, 2, 5, 4, 8};
+// int smallest = Sorter.smallest(array);
+// System.out.println("Smallest: " + smallest);
+//}
+public static void indexOfSmallest(){
+    int[] array = {3, 2, 5, 4, 8};
+    int index = findIndexOfSmallest(array);
+    System.out.println("Index of smallest number: " + index);
+}
+
+public static int findIndexOfSmallest(int[] array) {
+    int smallest = array[0];
+    int index = 0;
+    for (int i = 1; i < array.length; i++) {
+        if (array[i] < smallest) {
+            smallest = array[i];
+            index = i;
+        }
     }
+    return index;
+}
+
 }
--- src/main/java/rvt/DefaultController.java
+++ src/main/java/rvt/DefaultController.java
@@ -1,15 +1,69 @@
 package rvt;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+
+import ch.qos.logback.core.model.Model;
+import javax.validation.Valid;
+    
 
 @Controller
 public class DefaultController {
-    
-   @GetMapping(value = "/")
-    String index(@RequestParam(name="name", required=false, defaultValue="null") String name, Model model) {
-        return "index";
-    }
+String index(@RequestParam(name = "name", required = false, defaultValue = "null") String name, Model model){
+return "index";
+}
+
+@GetMapping(value = "/hello")
+String hello(){
+return "hello";
+}
+
+@GetMapping(value = "/about")
+ModelAndView about(){
+ModelAndView modelAndView = new ModelAndView("about");
+
+//Metode priekš koda testēšanas
+
+return modelAndView;
+}
+@GetMapping(value = "/Links")
+String Links(){
+return "Links";
+}
+@GetMapping(value = "/Experiment")
+String Experiment(){
+return "Experiment";
+}
+@GetMapping(value = "/test")
+public ModelAndView testAction(){
+ModelAndView modelAndView = new ModelAndView("test");
+return modelAndView;
+}
+@GetMapping(value = "/registration")
+public ModelAndView RegistartionPage(){
+ModelAndView modelAndView = new ModelAndView("registration");
+return modelAndView;
+}
+
+@PostMapping(value = "/registration")
+public String registration(
+@Valid @ModelAttribute("sudent") Student student,
+BindingResult bindingResult
+) {
+if (bindingResult.hasErrors()){
+return "registration/registration-page";
+}
+return "redirect:/register?succcess";
+}
 }
